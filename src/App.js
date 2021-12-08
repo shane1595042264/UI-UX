28eb2a908c60cd3e11c4d72e9f672a398321cc7c
Shane Li <61711097+shane1595042264@users.noreply.github.com>
2021-12-08 10:51:59 -0500

    setup again
    I reset my computer so I decided to create a new project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+import {Article, Brand, CTA } from './components';
+import {Footer, Header, Blog, WhatGPT3, Features, Possibility} from './containers';
+
+const App= () => {
+  return (
+    <div className="App">
+      <h1>Sign Language</h1>
+    </div>
+  );
+}
+
+export default App;
