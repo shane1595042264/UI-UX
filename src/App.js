209206178db2661108984510c8826ef25882f0c9
Shane Li <61711097+shane1595042264@users.noreply.github.com>
2021-12-14 10:03:14 -0500

    extend

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-import {Article, Brand, CTA } from './components';
+import {Article, Brand, CTA, Navbar } from './components';
 import {Footer, Header, Blog, WhatGPT3, Features, Possibility} from './containers';
 
 const App= () => {
   return (
     <div className="App">
-      <h1>Sign Language</h1>
+      <div className="gradient__bg">
+      <Navbar />
+      <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import './header.css'; 
+import people from '../../assests/people.png';
+import ai from '../../assests/ai.png';
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="gpt3__header section__padding" id="home">
+            <div className='gpt3__header-content'>
+            <h1 className='gradient__text'> 
+            Learn Sign Language with AI
+            </h1>
+            <p>
+                I got rejected from CMU.
+            </p>
+            <div className='gpt3__header-content__input'>
+                <input type='email' placeholder='Your Email Address'/>
+                <button type='button'>Get Started</button>
+            </div>
+            <div className='gpt3__header-content__people'>
+                <img src={people} />
+                <p>
+                    1600 people requested access in last 24 hours
+                </p>
+            </div>
+            </div>
+            
+            <div className='gpt3__header-image'>
+                <img src={ai}/>
+            </div>
         </div>
     )
 }
 
-export default Header
+export default Header;
